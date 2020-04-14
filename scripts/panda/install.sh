2cc085053e8601b69d202d84089e3f61fea8e21f
eternity-CSL <eternity@eternity-pilot.lan>
2020-04-14 16:32:21 -0700

    Added text to create blank vin file for others to edit

--- scripts/panda/install.sh
+++ scripts/panda/install.sh
@@ -4,6 +4,14 @@
 echo "========================="
 echo "Installing panda services"
 
+if [ ! -d /etc/libpanda.d ]; 
+	mkdir /etc/libpanda.d
+fi
+
+if [ ! -f /etc/libpanda.d/vin ]; then
+	touch /etc/libpanda.d/vin
+fi
+
 cp pandarecord.sh /usr/local/sbin/pandarecord
 
 cp pandasettime.service.txt  /etc/systemd/system/pandasettime.service
--- scripts/panda/pandarecord.sh
+++ scripts/panda/pandarecord.sh
@@ -3,9 +3,14 @@
 
 #cd ~/VersionControl/strym/examples/ && source ~/VirtualEnv/stream/bin/activate && python strym_impl.py
 FOLDER_PRE=/var/panda/CyverseData/JmscslgroupData/PandaData
+VIN=""
+# Note: does not remove spaces etc. should harden later TODO
+if [ -f /etc/libpanda.d/vin ]; then
+	VIN=_$(cat /etc/libpanda.d/vin)
+fi
 FOLDER_DATE=$(date +%Y_%m_%d)
 FOLDER=${FOLDER_PRE}/${FOLDER_DATE}
-FILENAME_PRE=$(date +%Y-%m-%d-%H-%M-%S)
+FILENAME_PRE=$(date +%Y-%m-%d-%H-%M-%S)${VIN}
 if [ ! -d ${FOLDER_PRE}/${FOLDER_DATE} ]; then
 	echo "Creating ${FOLDER_PRE}/${FOLDER_DATE}..."
 	mkdir -p ${FOLDER_PRE}/${FOLDER_DATE}
