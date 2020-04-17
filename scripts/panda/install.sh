39f8b4ef9b9b8c79d60447766b9c8b0e033273a7
blegas78 <12centdwarf@gmail.com>
2020-04-16 18:24:39 -0700

    Added wifi and VIN configuration to installation scripts.  hardware tutorial update in progress

--- scripts/panda/install.sh
+++ scripts/panda/install.sh
@@ -4,6 +4,8 @@
 echo "========================="
 echo "Installing panda services"
 
+echo "Configuring VIN file."
+
 if [ ! -d /etc/libpanda.d ]; then
 	mkdir /etc/libpanda.d
 fi
@@ -11,6 +13,27 @@ fi
 if [ ! -f /etc/libpanda.d/vin ]; then
 	touch /etc/libpanda.d/vin
 fi
+
+if [ ! -s /etc/libpanda.d/vin ]; then
+	choice=""
+
+	while [ "${choice}" != "n" ] && [ "${choice}" != "y" ]
+	do
+		read -n 1 -p " - VIN file is empty, enter VIN now? [y/n]:" choice
+		echo ""
+
+		echo "you entered: ${name}"
+	done
+
+	if [ "${choice}" = "y" ];
+	then
+		read -p " - Enter VIN:" vin
+
+		echo "${vin}" > /etc/libpanda.d/vin
+	fi
+fi
+
+echo "Configuring startup scripts"
 
 cp pandarecord.sh /usr/local/sbin/pandarecord
 
@@ -20,6 +43,8 @@ chmod 655 /etc/systemd/system/pandasettime.service
 chmod 655 /etc/systemd/system/pandarecord.service
 
 systemctl daemon-reload
+
+echo "Enabling startup scripts"
 
 systemctl enable pandasettime.service
 systemctl enable pandarecord.service
--- scripts/pi4wifi/install.sh
+++ scripts/pi4wifi/install.sh
@@ -82,5 +82,31 @@ systemctl daemon-reload
 systemctl enable crazypifi.service
 
 
+# Setup Wifi AP:
+
+name=""
+while [ "${name}" != "n" ] && [ "${name}" != "y" ]
+do
+	read -n 1 -p " - Configure WiFi now? [y/n]:" name
+	echo ""
+
+	echo "you entered: ${name}"
+done
+
+if [ "${name}" = "y" ];
+then
+	read -p " - Enter SSID (WiFi name):" ssid
+
+	read -sp " - Enter passphrase:" passphrase
+	echo
+
+	echo "Configuring WPA Supplicant..."
+
+	wpa_passphrase ${ssid} ${passphrase} >> /etc/wpa_supplicant/wpa_supplicant.conf
+fi
+
+unset passphrase
+
+
 echo "Done."
 echo "----------------------------"
