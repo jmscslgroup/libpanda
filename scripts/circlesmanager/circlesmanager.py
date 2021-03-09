5613840218b27737fe73fba9c128dbdf6d8034dc
blegas78 <12centdwarf@gmail.com>
2021-03-09 19:31:13 +0000

    scripts will now only shutdown system if power has been disconnected for a duration of 10 minutes.  Power reconnects reset this time

--- scripts/circlesmanager/circlesmanager.py
+++ scripts/circlesmanager/circlesmanager.py
@@ -26,41 +26,53 @@ def getFileContents( filename ):
 	#	print("From " + filename + " Read: " + contents )
 	return contents
 
-def loop():
-	logging.info("in loop()")
+class CirclesManager:
+	def __init__(self, powerOffTime=60):
+		self.powerOffTimeoutInSeconds = powerOffTime
+		self.powerDisconnectTime = 0
 
-	try:
-		hasInternet = int(getFileContents( fileInternet ))
-		isApClient = int(getFileContents( fileIsApClient ))
-		isApHost = int(getFileContents( fileisApHost ))
-		hasApClients = int(getFileContents( fileHasApClients ))
+	def loop(self):
+		logging.info("in loop()")
 
-		hasExternalPower = bool(int(getFileContents( fileX725HasExtenalPower )))
-		batteryCurrent = float(getFileContents( fileX725BatteryCurrent ))
-		batteryVoltage = float(getFileContents( fileX725BatteryVoltage ))
-		capacity = float(getFileContents( fileX725Capacity ))
-	except Exception as e:
-		logging.info(e)
-		return
+		try:
+			hasInternet = int(getFileContents( fileInternet ))
+			isApClient = int(getFileContents( fileIsApClient ))
+			isApHost = int(getFileContents( fileisApHost ))
+			hasApClients = int(getFileContents( fileHasApClients ))
 
-	logging.info("Battery Voltage: " + str(batteryVoltage))
-	logging.info("External Power:  " + str(hasExternalPower))
+			hasExternalPower = bool(int(getFileContents( fileX725HasExtenalPower )))
+			batteryCurrent = float(getFileContents( fileX725BatteryCurrent ))
+			batteryVoltage = float(getFileContents( fileX725BatteryVoltage ))
+			capacity = float(getFileContents( fileX725Capacity ))
+		except Exception as e:
+			logging.info(e)
+			return
 
-	if not hasExternalPower:
-		logging.info(" - Power Disconnected!  Running final scripts...")
-		os.system("echo \"- - This is where we run a script\"")
-		logging.info(" - Shutting system down...")
-		f = open(fileX725HasExtenalPower, "w")
-		f.write("1")
-		f.close()
-		os.system("x725shutdown")
+		logging.info("Battery Voltage: " + str(batteryVoltage))
+		logging.info("External Power:  " + str(hasExternalPower))
 
+		if not hasExternalPower:
+			self.powerDisconnectTime += 1.0/updateRate
+			logging.info(" - Power disconnected, shutting down in " + str(self.powerOffTimeoutInSeconds - self.powerDisconnectTime))
+			if self.powerDisconnectTime >= self.powerOffTimeoutInSeconds:
+				logging.info(" - - Power disconnect timeout!  Running final scripts...")
+				os.system("echo \"- - - This is where we run a script\"")
+				logging.info(" - - Shutting system down...")
+				f = open(fileX725HasExtenalPower, "w")
+				f.write("1")
+				f.close()
+				os.system("x725shutdown")
+		else:
+			self.powerDisconnectTime = 0
 
 if __name__ == "__main__":
 	f = open(fileX725HasExtenalPower, "w")
 	f.write("1")
 	f.close()
+
+	myCirclesManager = CirclesManager(60*10);
+
 	while True:
 		time.sleep(1.0/updateRate)
-		loop()
+		myCirclesManager.loop()
 
