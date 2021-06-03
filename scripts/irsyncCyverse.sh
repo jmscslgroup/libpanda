4a646706b8cbd3f4e276f3b4be0985ea204231f8
Jonathan Sprinkle <sprinkjm@email.arizona.edu>
2021-06-03 11:34:18 -0700

    Added option to force upload without asking by -f or -y

--- scripts/irsyncCyverse.sh
+++ scripts/irsyncCyverse.sh
@@ -3,11 +3,25 @@ VINFILE=/etc/libpanda.d/vin
 VIN=$(cat ${VINFILE})
 LOCAL=/var/panda/CyverseData/JmscslgroupData/PandaData
 REMOTE=/iplant/home/sprinkjm/private-circles/${VIN}/libpanda
+response=
+
+while getopts “hf” opt; do
+  case $opt in
+    h) echo "Uploads data to CyVerse."
+	echo " -f : force upload without prompt" 
+	;;
+    f) response=y;;
+    y) response=y;;
+  esac
+done
 
 if [ -s ${VINFILE} ]
 then
 
-read -r -p "Would you like to push data to $REMOTE? [y/N] " response
+if [ -z "$response" ]; then
+	read -r -p "Would you like to push data to $REMOTE? [y/N] " response
+fi
+
 case "$response" in
       [yY][eE][sS]|[yY])
 	  irsync -r -v ${LOCAL} i:${REMOTE}
