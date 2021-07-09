35b0cba57cd332ca4b05b62faced250edb4f7639
blegas78 <12centdwarf@gmail.com>
2021-07-09 11:16:08 -0700

    installation sets permissions for bagfile directories to eliminate need to run ros nodes as root

--- install.sh
+++ install.sh
@@ -27,6 +27,29 @@ cd ../..
 systemctl enable ssh
 systemctl start ssh
 
+
 #cp scripts/addWifiAp.sh ../
 #../setVin.sh
 #../addWifiAp.sh
+
+
+# Build the bagfile directory:
+BAGFILE_DIR=/var/panda/CyverseData/JmscslgroupData/bagfiles
+
+if [ ! -d ${BAGFILE_DIR} ]; then
+	echo "Creating ${BAGFILE_DIR}..."
+	mkdir -p ${BAGFILE_DIR}
+fi
+
+# The following is authored by Jonathan Sprinkle:
+# 1. setting the permissions of bagfiles folder to be in the dialout group (circles is a member of dialout):
+cd /var/panda/CyverseData/JmscslgroupData/bagfiles
+sudo chown -R :dialout .
+
+# 2. Make the bagfiles folder and all contained folders writeable by group membership:
+sudo chmod -R g+w .
+
+# 3. Set the group id permissions to be carried forward for any folders created in bagfiles:
+
+sudo chmod g+s .
+# With these changes in place (which can be done during install for the libpanda stuff) we can have everything such that you do not need to be sudo su to write to the bagfiles.
