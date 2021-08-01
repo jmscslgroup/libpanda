97b9d6d0f42f202ee7cf2f8eb91892a35f6f5efd
Matthew Nice <mnice@Matthews-MacBook-Pro-8.local>
2021-08-01 17:05:58 -0500

    Update blinktStatus.py

--- scripts/blinkt/blinktStatus.py
+++ scripts/blinkt/blinktStatus.py
@@ -7,7 +7,7 @@ from blinkt import set_pixel, show
 pi = 3.1415926535897
 
 
-maxLevel = 0.250	# Max = 1.0, 0.0 = off
+maxLevel = 0.10	# Max = 1.0, 0.0 = off
 updateRate = 2.0	# Hz
 
 fileInternet = "/etc/libpanda.d/hasinternet"
@@ -77,24 +77,24 @@ while True:
 # LED 0: Constantly changing to show that blinkt script is running
 	hue = (time.time()*32.0 % 360)/360.0
 	r, g, b = [int(pow(c,2.2) * 255) for c in colorsys.hsv_to_rgb(hue, 1, maxLevel)]
-	set_pixel(0, r, g, b)
+	set_pixel(0, r, g, b, maxLevel)
 
 # LED 1
 #	print("Has Internet: " + str(hasInternet) )
 	#blue = has internet, red otherwise
 	if hasInternet:
-		set_pixel(1, 0, 0, int(255.0*maxLevel))
+		set_pixel(1, 0, 0, int(255.0*maxLevel),maxLevel)
 	else:
-		set_pixel(1, int(255.0*maxLevel), 0, 0)
+		set_pixel(1, int(255.0*maxLevel), 0, 0,maxLevel)
 
 # LED 2
 # blue for is host, green for is client, red for neither
 	if isApHost:
-		set_pixel(2, 0, 0, int(255.0*maxLevel))
+		set_pixel(2, 0, 0, int(255.0*maxLevel),maxLevel)
 	elif isApClient:
-		set_pixel(2, 0, int(255.0*maxLevel), 0)
+		set_pixel(2, 0, int(255.0*maxLevel), 0,maxLevel)
 	else:
-		set_pixel(2, int(255.0*maxLevel), 0, 0)
+		set_pixel(2, int(255.0*maxLevel), 0, 0,maxLevel)
 
 # LED 3:
 # purple for having clients, off otherwise
@@ -104,11 +104,11 @@ while True:
 #	else:
 #		set_pixel(3, 0, 0, 0)
 	if controlAllowed == 1:
-                set_pixel(3,0,200,0,maxLevel) #if controls on and publishing: GREEN
+                set_pixel(3,0,255*maxLevel,0,maxLevel) #if controls on and publishing: GREEN
 	elif controlAllowed == 0:
-            set_pixel(3,0,0,200,maxLevel) #if controls off but publishing: blue
+            set_pixel(3,0,0,255*maxLevel,maxLevel) #if controls off but publishing: blue
 	elif controlAllowed == -1:
-            set_pixel(3,200,0,0,maxLevel) #if not published within 3 seconds: RED
+            set_pixel(3,255*maxLevel,0,0,maxLevel) #if not published within 3 seconds: RED
 # LED 4:
 #	newCapacity = max(min(capacity, 100.0), 0.0)
 #	hue = newCapacity/100.0 * 120.0/360.0	# green = charged, red = discharged
@@ -131,12 +131,12 @@ while True:
 #	set_pixel(6, r, g, b)
 
 	if pandaRecording == 1:
-		set_pixel(6, 0, 0, int(255.0*maxLevel))
+		set_pixel(6, 0, 0, int(255.0*maxLevel),maxLevel)
 	elif pandaRecording == 0:
-		set_pixel(6, int(255.0*maxLevel)*blinkRecord, 0, 0)
+		set_pixel(6, int(255.0*maxLevel)*blinkRecord, 0, 0,maxLevel)
 		blinkRecord = not blinkRecord
 	else:	# not active or other error
-		set_pixel(6, 0, 0, 0);
+		set_pixel(6, 0, 0, 0,maxLevel);
 
 # LED 7: Red: no panda GPS, blue: GPS time synced
 	#if hasExternalPower:
@@ -144,11 +144,11 @@ while True:
 	#else:
 	#	set_pixel(7, int(255.0*maxLevel), 0, 0)
 	if pandaGps == 1:
-		set_pixel(7, 0, 0, int(255.0*maxLevel))
+		set_pixel(7, 0, 0, int(255.0*maxLevel),maxLevel)
 	elif pandaGps == 0:
-		set_pixel(7, int(255.0*maxLevel)*blinkGps, 0, 0)
+		set_pixel(7, int(255.0*maxLevel)*blinkGps, 0, 0,maxLevel)
 		blinkGps = not blinkGps
 	else:
-		set_pixel(7, 0, 0, 0)
+		set_pixel(7, 0, 0, 0,maxLevel)
 
 	show()
