e758a92501799131f60372bc807b3359d6b12d9a
blegas78 <12centdwarf@gmail.com>
2021-08-03 18:24:19 -0500

    Changed the controller last minute to reflect the rl0179 instead of micromodelv2

--- scripts/experiments/run0804_controller.sh
+++ scripts/experiments/run0804_controller.sh
@@ -2,4 +2,6 @@
 
 cd /home/circles/catkin_ws/
 source devel/setup.bash
-roslaunch transfer_pkg micromodelv2_readonly.launch hwil:=true readonly:=false
+#roslaunch transfer_pkg micromodelv2_readonly.launch hwil:=true readonly:=false
+# The anobve controller was changed last minute to the following:
+roslaunch transfer_pkg rl0719_readonly.launch hwil:=true readonly:=false
