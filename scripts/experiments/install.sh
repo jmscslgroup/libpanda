6e232e6ab457eed871c2ceca9efb06a9e3556e3c
blegas78 <12centdwarf@gmail.com>
2021-08-05 16:17:20 -0500

    Changes to the followerstopper are now reflected in the new experiment scripts

--- scripts/experiments/install.sh
+++ scripts/experiments/install.sh
@@ -7,3 +7,4 @@ echo "Installing experiment scripts!"
 cp monitor0802_controller.sh /home/circles/monitor.sh
 #cp run0804_controller.sh /home/circles/
 cp run0806_controller.sh /home/circles/
+cp run0806_controller_phase_2.sh /home/circles/
--- scripts/experiments/run0806_controller.sh
+++ scripts/experiments/run0806_controller.sh
@@ -3,4 +3,5 @@
 cd /home/circles/catkin_ws/
 source devel/setup.bash
 
-roslaunch transfer_pkg rl0805_readonly.launch hwil:=true readonly:=false
+#roslaunch transfer_pkg rl0805_readonly.launch hwil:=true readonly:=false
+roslaunch transfer_pkg rl0805_v3_readonly.launch hwil:=true readonly:=false
