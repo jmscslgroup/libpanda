39841ef2dd5ebb780fcd49be7ee9ac45124b038a
Rahul Bhadani <rahulbhadani@email.arizona.edu>
2022-08-31 14:46:12 -0500

    Install script for ACC

---
+++ scripts/install_acc_accel.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+# Author: Matt Bunting
+
+echo "----------------------------"
+if [[ $EUID == 0 ]];
+  then echo "Do NOT run this script as root"
+  exit
+fi
+
+echo "Installing/Updating Vandertest ROS packages"
+
+source ~/.bashrc
+
+cd ~
+if [ ! -d catkin_ws/src ]; then
+	mkdir -p catkin_ws/src
+fi
+cd catkin_ws/src
+
+# The following are repositories under jmscslgroup:
+declare -a repositories=(can_to_ros transfer_pkg  ghostfollower ghostfollower_max velocity_controller ghost_mode velocityramp setpointreader acc_accel)
+
+for repository in "${repositories[@]}"
+do
+	echo "Checking" $repository
+	if [ -d ${repository} ]; then
+		cd ${repository}
+		git pull
+		cd ..
+	else
+		git clone "https://github.com/jmscslgroup/${repository}.git"
+	fi
+done
+
+
+cd ~/catkin_ws
+catkin_make
+
+source devel/setup.sh
+rosrun robot_upstart uninstall can
+rosrun robot_upstart install transfer_pkg/launch/acc_accel.launch --user root
+
+
+
+echo "Enabling can_to_ros startup script"
+sudo systemctl daemon-reload
+sudo systemctl enable can
+
+echo "----------------------------"
