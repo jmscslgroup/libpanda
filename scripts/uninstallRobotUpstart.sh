b5338d3c5146d56072014ae253dbf8529e554be9
Jonathan Sprinkle <jonathan.sprinkle@vanderbilt.edu>
2022-09-30 16:04:55 +0100

    Ugh, the uninstall piece was commented out in the committed version.

--- scripts/uninstallRobotUpstart.sh
+++ scripts/uninstallRobotUpstart.sh
@@ -14,7 +14,7 @@ fi
 # check to see if can.service is installed
 if [ "x"`systemctl list-units | grep -c can.service` = "x1" ]; then
 	echo "Uninstalling can.service"
-#	rosrun robot_upstart uninstall can
+	rosrun robot_upstart uninstall can
 fi
 
 echo "Done uninstalling services"
