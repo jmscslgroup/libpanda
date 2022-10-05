d23742ad18714e3d51aab609ee1a3a6ab13b3d76
Jonathan Sprinkle <jonathan.sprinkle@vanderbilt.edu>
2022-10-05 16:32:22 +0100

    trying a new approach for update/install files

--- scripts/install_rl_cruise_planner.sh
+++ scripts/install_rl_cruise_planner.sh
@@ -1,64 +1,28 @@
 #!/bin/bash
 # Author: Matt Bunting
 
+# if you follow conventions correctly, you only need to change the below two parameters
+
+UPDATE_SCRIPT=update_rl_cruise_planner.sh
+LAUNCH_FILE=rl_cruise_planner_readonly.launch
+
 echo "----------------------------"
 if [[ $EUID == 0 ]];
   then echo "Do NOT run this script as root"
   exit
 fi
 
-echo "Installing/Updating Vandertest ROS packages"
+# run the update script
+./${UPDATE_SCRIPT}
 
-source ~/.bashrc
-
-sh uninstallRobotUpstart.sh
-
-cd ~
-if [ ! -d catkin_ws/src ]; then
-	mkdir -p catkin_ws/src
-fi
-cd catkin_ws/src
-
-# The following are repositories under jmscslgroup:
-declare -a repositories=(can_to_ros  ghostfollower ghostfollower_max velocity_controller ghost_mode velocityramp setpointreader acc_settings)
-
-for repository in "${repositories[@]}"
-do
-	echo "Checking" $repository
-	if [ -d ${repository} ]; then
-		cd ${repository}
-		git pull
-		cd ..
-	else
-		git clone "https://github.com/jmscslgroup/${repository}.git"
-	fi
-done
-
-# this requires credentials:
-if [ -d algos-stack ]; then
-	cd algos-stack
-	git pull
-	cd ..
-else
-	git clone https://github.com/CIRCLES-consortium/algos-stack
-fi
-cd algos-stack
-#git checkout swil_rahul
-#git checkout setpoint_rahul
-git checkout rl-cruise
-
-
-cd ~/catkin_ws
-catkin_make
-
+pushd ~/catkin_ws
 source devel/setup.sh
 #rosrun robot_upstart uninstall can
-rosrun robot_upstart install can_to_ros/launch/rl_cruise_planner_readonly.launch --user root
-
-
+rosrun robot_upstart install can_to_ros/launch/${LAUNCH_FILE} --user root
 
 echo "Enabling can_to_ros startup script"
 sudo systemctl daemon-reload
 sudo systemctl enable can
+popd
 
 echo "----------------------------"
