6d9199e3609075712709600d3107ac495cfaa76f
Matthew Nice <matthewwalternice@gmail.com>
2022-10-11 19:41:39 +0100

    adding update time to piStatus

--- scripts/piStatus/getPiStatus.sh
+++ scripts/piStatus/getPiStatus.sh
@@ -8,10 +8,11 @@ do
   WLAN0_MAC=$(ifconfig wlan0 | grep -Eo 'ether (addr:)?([0-9a-f]*\:){5}[0-9a-f]*' | grep -Eo '([0-9a-f]*\:){5}[0-9a-f]*')
   VIN=$(cat /etc/libpanda.d/vin)
   EXTERNAL_POWER=$(echo $(cat /etc/libpanda.d/x725hasexternalpower)|tr -d '\r')
+  UPDATE_TIME=$(date +%s)
 
-  JSON='{"wlan0_mac":"%s","wlan0_ip":"%s","eth0_ip":"%s","vin":"%s","external_power":%d}'
+  JSON='{"wlan0_mac":"%s","wlan0_ip":"%s","eth0_ip":"%s","vin":"%s","external_power":%d,"update_time":"%s"}'
 
-  printf "$JSON" "$WLAN0_MAC" "$WLAN0_IP" "$ETH0_IP" "$VIN" "$EXTERNAL_POWER" > /etc/libpanda.d/piStatus.json
+  printf "$JSON" "$WLAN0_MAC" "$WLAN0_IP" "$ETH0_IP" "$VIN" "$EXTERNAL_POWER" "$UPDATE_TIME" > /etc/libpanda.d/piStatus.json
 
   sleep 10
 done
