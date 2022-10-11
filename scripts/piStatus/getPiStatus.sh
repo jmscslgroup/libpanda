166fb1e93de45aa34a7bf943d48cf9468023354f
Matthew Nice <matthewwalternice@gmail.com>
2022-10-11 22:53:46 +0100

    adding many more fields in piStatus to check in on RAM and system memory

--- scripts/piStatus/getPiStatus.sh
+++ scripts/piStatus/getPiStatus.sh
@@ -9,10 +9,21 @@ do
   VIN=$(cat /etc/libpanda.d/vin)
   EXTERNAL_POWER=$(echo $(cat /etc/libpanda.d/x725hasexternalpower)|tr -d '\r')
   UPDATE_TIME=$(date +%s)
+#  FREE_RAM=$(free -h | grep 'Mem' | awk '{print $7}')
+#  TOTAL_MEMORY=$(awk '/VmallocTotal/ { printf "%.3f \n", $2/1024/1024 }' /proc/meminfo)
+#  USED_MEMORY=$(awk '/VmallocUsed/ { printf "%.3f \n", $2/1024/1024 }' /proc/meminfo)
+  FREE_RAM=$(awk '/MemFree/ { printf "%.3fG", $2/1024/1024 }' /proc/meminfo)
+  AVAILABLE_RAM=$(awk '/MemAvailable/ { printf "%.3fG", $2/1024/1024 }' /proc/meminfo)
+  TOTAL_RAM=$(awk '/MemTotal/ { printf "%.3fG", $2/1024/1024 }' /proc/meminfo)
+  TOTAL_MEMORY=$(lsblk -fmo NAME,FSUSE%,FSAVAIL,FSSIZE | grep 'mmcblk0p2' | awk '{printf "%.2fG", $4 }')
+  TOTAL_MEMORY_USED=$(lsblk -fmo NAME,FSUSE%,FSAVAIL,FSSIZE | grep 'mmcblk0p2' | awk '{printf "%.2fG", $3 }')
+  TOTAL_MEMORY_USED_PERCENT=$(lsblk -fmo NAME,FSUSE%,FSAVAIL,FSSIZE | grep 'mmcblk0p2' | awk '{printf "%.1f", $2 }')
 
-  JSON='{"wlan0_mac":"%s","wlan0_ip":"%s","eth0_ip":"%s","vin":"%s","external_power":%d,"update_time":"%s"}'
 
-  printf "$JSON" "$WLAN0_MAC" "$WLAN0_IP" "$ETH0_IP" "$VIN" "$EXTERNAL_POWER" "$UPDATE_TIME" > /etc/libpanda.d/piStatus.json
+  JSON='{"wlan0_mac":"%s","update_time":"%d","wlan0_ip":"%s","eth0_ip":"%s","vin":"%s","external_power":"%d","free_ram":"%s","available_ram":"%s","total_ram":"%s","total_memory_used":"%s","total_memory":"%s","total_memory_used_percent":"%s"}'
+
+
+  printf "$JSON" "$WLAN0_MAC" "$UPDATE_TIME" "$WLAN0_IP" "$ETH0_IP" "$VIN" "$EXTERNAL_POWER" "$FREE_RAM" "$AVAILABLE_RAM" "$TOTAL_RAM" "$TOTAL_MEMORY_USED" "$TOTAL_MEMORY" "$TOTAL_MEMORY_USED_PERCENT" > /etc/libpanda.d/piStatus.json
 
   sleep 10
 done
