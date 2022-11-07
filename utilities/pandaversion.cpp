7d6bad659bf94cd8a898a64406ae481eafd42d39
blegas78 <12centdwarf@gmail.com>
2022-11-07 10:36:23 -0600

    New utility: pandaversion resurns the hash of the current installed version of libpanda

---
+++ utilities/pandaversion.cpp
@@ -0,0 +1,13 @@
+/*
+ Author: Matt Bunting
+ */
+
+#include <iostream>
+
+#include "panda_version_i.h"
+
+int main(int argc, char **argv) {
+	std::cout << LIBPANDA_VERSION << std::endl;
+
+	return 0;
+}
