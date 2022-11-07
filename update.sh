04efb2f1a02c9019bc4307570ae3547ccad76bed
blegas78 <12centdwarf@gmail.com>
2022-11-07 11:33:55 -0600

    Scratch the prior commit on error logging update.sh, that didn't help.  Bug fixed anyway

--- update.sh
+++ update.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Author: Matt Bunting
 
->&2 echo "Running libpanda's update.sh"
+echo "Running libpanda's update.sh"
 
 if [[ $EUID == 0 ]];
-  then >&2 echo "Do NOT run this script as root"
+  then echo "Do NOT run this script as root"
   exit
 fi
 
@@ -18,25 +18,25 @@ CURRENT_LIBPANDA_GIT_VERSION=$(git rev-parse HEAD | tr -d "\n\r")
 
 if [ "$INSTALLED_LIBPANDA_GIT_VERSION" = "$CURRENT_LIBPANDA_GIT_VERSION" ];
 then
-	>&2 echo "libpanda is already up to date!"
+	echo "libpanda is already up to date!"
 	exit 1
 fi
 
 
->&2 echo "Stopping can_to_ros..."
+echo "Stopping can_to_ros..."
 sudo systemctl stop can
 
->&2 echo "Updating libpanda..."
+echo "Updating libpanda..."
 ./install.sh
 
 cd scripts
->&2 echo "Executing installMVTRosPackages..."
+echo "Executing installMVTRosPackages..."
 ./installMVTRosPackages.sh
 
->&2 echo "Executing install_rl_cruise_hybrid_planner..."
+echo "Executing install_rl_cruise_hybrid_planner..."
 ./install_rl_cruise_hybrid_planner.sh
 
->&2 echo "Starting can_to_ros..."
+echo "Starting can_to_ros..."
 sudo systemctl start can
 
->&2 echo "libpanda update.sh done."
+echo "libpanda update.sh done."
