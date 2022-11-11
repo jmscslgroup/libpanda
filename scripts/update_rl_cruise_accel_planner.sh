a7204f469253b98c46f8e467ccb3cd94bde055f9
Kathy Jang <kathyjang@gmail.com>
2022-11-11 17:50:55 -0600

    Added install and update scripts for RL cruise planner

---
+++ scripts/update_rl_cruise_accel_planner.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+# Author: Matt Bunting
+
+# Change params below, assuming you followed conventions, this should be
+# all you need to change IF YOU ARE USING ONNX MODELS for your controller
+
+PRETTY_STRING="RL Cruise Accel-based Planner"
+ALGOS_STACK_BRANCH=rl_cruise_accel
+
+echo "----------------------------"
+if [[ $EUID == 0 ]];
+  then echo "Do NOT run this script as root"
+  exit
+fi
+
+echo "Installing/Updating ${PRETTY_STRING} packages"
+
+source ~/.bashrc
+
+sh uninstallRobotUpstart.sh
+
+#cd ~
+#if [ ! -d catkin_ws/src ]; then
+#	mkdir -p catkin_ws/src
+#fi
+#cd catkin_ws/src
+#
+## The following are repositories under jmscslgroup:
+#declare -a repositories=(can_to_ros live_tracker)
+#
+#for repository in "${repositories[@]}"
+#do
+#	echo "Checking" $repository
+#	if [ -d ${repository} ]; then
+#		cd ${repository}
+#		git pull
+#		cd ..
+#	else
+#		git clone "https://github.com/jmscslgroup/${repository}.git"
+#	fi
+#done
+#
+## this requires credentials:
+#if [ -d algos-stack ]; then
+#	cd algos-stack
+#	git pull
+#	cd ..
+#else
+#	git clone https://github.com/CIRCLES-consortium/algos-stack
+#fi
+#cd algos-stack
+## checkout the repository branch we need
+#git checkout ${ALGOS_STACK_BRANCH}
+#
+#cd ~/catkin_ws
+#catkin_make
+
+echo "----------------------------"
+
