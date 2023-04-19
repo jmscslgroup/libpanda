b0fef090f877b837d103b53d958dbacbf924e352
blegas78 <12centdwarf@gmail.com>
2023-04-19 14:17:08 -0500

    More fixes to simpleSend install

--- apps/apps/simpleSend/install.sh
+++ apps/apps/simpleSend/install.sh
@@ -5,11 +5,11 @@ echo "Installing App simpleSend"
 
 # Here is where we perform installation of scripts, services, etc.
 echo " - Installing scripts..."
-cp simplesend.service.txt /etc/systemd/system/simplesend.service
+cp /etc/libpanda.d/apps/simpleSend/simplesend.service.txt /etc/systemd/system/simplesend.service
 chmod 655 /etc/systemd/system/simplesend.service
 
 # Enable the installed services on boot:
 echo " - Enabling startup scripts..."
-#systemctl daemon-reload # if needed
+systemctl daemon-reload # if needed
 systemctl enable simplesend
 
