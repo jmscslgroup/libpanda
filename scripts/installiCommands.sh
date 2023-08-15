254ec648d4e346c072b500e891dab7598f92b603
blegas78 <12centdwarf@gmail.com>
2023-08-15 12:19:34 -0500

    untested iCommand install script added

---
+++ scripts/installiCommands.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Author: Matt Bunting
+
+# installs iCommands and does some nasty stuff with dpkg and ssl
+
+echo "----------------------------"
+if [[ $EUID == 0 ]];
+  then echo "Do NOT run this script as root"
+  exit
+fi
+
+SRC_DIR=$(cat /etc/libpanda.d/libpanda_src_dir)
+USER=$(cat /etc/libpanda.d/libpanda_usr)
+
+if [ ! -d ~/irods-icommands-debs ]; then
+    echo "Installing irods icommands..."
+    tar xzf $SRC_DIR/scripts/irods-icommands-debs.tgz -C /home/$USER/
+
+    cd /home/$USER/irods-icommands-debs
+    ./install.sh
+    # the following needs to run AFTER installing irods commands
+    sudo sed -i 's/libssl1.0.0/libssl1.1/g' /var/lib/dpkg/status
+else
+    echo "irods icommands already installed."
+fi
+
+echo "----------------------------"
