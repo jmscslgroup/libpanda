3ada7e4d4c5c6dbf5bd141e50195a905991657fe
blegas78 <12centdwarf@gmail.com>
2023-09-18 22:38:55 -0500

    ROS install script includes tf2, tested to work on fresh raspbian images

--- scripts/installROSNoetic.sh
+++ scripts/installROSNoetic.sh
@@ -14,23 +14,35 @@ if [[ $EUID == 0 ]];
   exit
 fi
 
-echo "Installing ROS Noetic"
+PURPLE='\033[0;35m'
+NC='\033[0m' # No Color
+
+echo -e "${PURPLE}Installing ROS Noetic${NC}"
 
 
-# These steps are copied from here, folling Buster: http://wiki.ros.org/ROSberryPi/Installing%20ROS%20Melodic%20on%20the%20Raspberry%20Pi
+# These steps are copied from here, following Buster: http://wiki.ros.org/ROSberryPi/Installing%20ROS%20Melodic%20on%20the%20Raspberry%20Pi
 
-# Noetic install: the above link was modified with information frm the following: http://wiki.ros.org/Installation/Source
+# Noetic install: the above link was modified with information from the following: http://wiki.ros.org/Installation/Source
 
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+#sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+# The raspbian release "bullseye" is not supported by ROS, so we hardcode "buster" instead of the more correct above line
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu buster main" > /etc/apt/sources.list.d/ros-latest.list'
+
+
 #sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 sudo -E apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 
+echo -e "${PURPLE}Running apt-get update/upgrade${NC}"
 sudo apt-get update
 sudo apt-get upgrade -y
 
 #sudo apt install -y python-rosdep python-rosinstall-generator python-wstool python-rosinstall build-essential cmake
 #sudo apt install -y python-rosdep python-rosinstall-generator python-wstool python-rosinstall build-essential cmake python-empy python-setuptools catkin # python3-catkin python3-empy python3-catkin-pkg python3-yaml
-pip3 install vcstool
+pip3 install python3-pip vcstool
+
+# note, the following will fail when running this updated script.  To fix:
+# sudo apt remove python3-rosdep python3-rosdep-modules python3-rosinstall-generator python3-rosinstall python3-catkin-pkg python3-catkin
+# sudo apt remove python3-rosdep python3-rosinstall-generator python3-rosinstall python3-catkin-pkg python3-catkin python3-rosdep2 python3-rosdistro python3-rospkg
 sudo apt-get install -y python3-rosdep python3-rosinstall-generator build-essential cmake python3-wstool python3-rosinstall liblog4cxx-dev
 #extras for noetic:
 sudo apt-get install -y python3-empy python3-catkin python3-catkin-pkg python3-yaml python3-rosdep
@@ -38,6 +50,7 @@ sudo apt-get install -y python3-empy python3-catkin python3-catkin-pkg python3-y
 sudo rosdep init
 rosdep update
 
+echo -e "${PURPLE}Making Workspace ~/ros_install_ws${NC}"
 mkdir -p ~/ros_install_ws
 cd ~/ros_install_ws
 
@@ -48,13 +61,22 @@ if [ -f src/.rosinstall ]; then
         rm src/.rosinstall
 fi
 
+
+echo -e "${PURPLE}Executing rosinstall_generator${NC}"
 #rosinstall_generator ros_comm --rosdistro melodic --deps --wet-only --tar > melodic-ros_comm-wet.rosinstall
 # The below is edited from above simply to include common_msgs
 #rosinstall_generator ros_comm common_msgs robot_upstart --rosdistro melodic --deps --wet-only --tar > melodic-ros_comm-wet.rosinstall
-rosinstall_generator ros_comm common_msgs robot_upstart --rosdistro noetic --deps --wet-only --tar > noetic-ros_comm-wet.rosinstall
+#rosinstall_generator tf2_tools tf2 robot_localization ros_comm common_msgs robot_upstart --rosdistro noetic --deps --wet-only --tar > noetic-ros_comm-wet.rosinstall
+rosinstall_generator geodesy actionlib tf2_tools tf2 tf robot_localization ros_comm common_msgs robot_upstart --rosdistro noetic --deps --wet-only --tar > noetic-ros_comm-wet.rosinstall
 #wstool init src melodic-ros_comm-wet.rosinstall
+
+# wstool breaks, fix by rolling back pyyaml:
+# pip3 install pyyaml==5.4.1
+echo -e "${PURPLE}Executing wstool${NC}"
 wstool init src noetic-ros_comm-wet.rosinstall
 
+
+echo -e "${PURPLE}Executing rosdep install (time for a coffee break)${NC}"
 cd ~/ros_install_ws
 #rosdep install -y --from-paths src --ignore-src --rosdistro noetic -r --os=debian:buster
 rosdep install -y --from-paths src --ignore-src --rosdistro noetic -r #--os=debian:buster
@@ -66,6 +88,8 @@ rosdep install -y --from-paths src --ignore-src --rosdistro noetic -r #--os=debi
 #git clone https://github.com/ros/common_msgs.git
 #cd ..
 
+
+echo -e "${PURPLE}Executing catkin_make_isolated (time for a coffee break x100)${NC}"
 #sudo ./src/catkin/bin/catkin_make_isolated --install -DCMAKE_BUILD_TYPE=Release --install-space /opt/ros/melodic -j4 #-DPYTHON_EXECUTABLE=/usr/bin/python3
 sudo ./src/catkin/bin/catkin_make_isolated --install -DCMAKE_BUILD_TYPE=Release --install-space /opt/ros/noetic -j4 -DPYTHON_EXECUTABLE=/usr/bin/python3
 # Noetic note: Setting python3 is needed for catkin_make_isolated to fix python3 dependency issues
